c2d1d6becf64c2c463d2d483dc83c6cd7ccec793
rahul verma <rahul.verma40@freecharge.com>
2024-01-24 22:54:36 +0530

    Add react-router-dom and related dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,64 @@
 import Home from "./pages/home/Home"
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet
+} from "react-router-dom";
+import Users from "./pages/users/Users";
+import Products from "./pages/products/Products";
+import Navbar from "./components/menu/Menu";
+import Footer from "./components/footer/Footer";
+import Menu from "./components/navbar/Navbar";
+import Login from "./pages/login/Login";
+import "./styles/global.scss"
 
 function App() {
 
+  const Layout = () => {
+    return (
+      <div className="main">
+        <Navbar />
+        <div className="container">
+          <div className="menuContainer">
+            <Menu />
+          </div>
+          <div className="contentContainer">
+            <Outlet />
+          </div>
+
+        </div>
+        <Footer />
+      </div>
+    )
+  }
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />
+        },
+        {
+          path: "/users",
+          element: <Users />
+        },
+        {
+          path: "/products",
+          element: <Products />
+        }
+      ],
+    },
+    {
+      path: "/login",
+      element: <Login />
+    },
+  ]);
+
   return (
-    <div><Home /></div>
+    <RouterProvider router={router} />
   )
 }
 
