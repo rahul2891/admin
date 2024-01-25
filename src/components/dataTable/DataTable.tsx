03aba16ed26f9a50c261cf790440702c954c2b76
rahul verma <rahul.verma40@freecharge.com>
2024-01-25 14:02:07 +0530

    made common components add & dataTable for users and products

--- src/components/dataTable/DataTable.tsx
+++ src/components/dataTable/DataTable.tsx
@@ -1,81 +1,49 @@
-import { DataGrid, GridColDef, GridToolbar, GridValueGetterParams } from "@mui/x-data-grid";
+import { DataGrid, GridColDef, GridToolbar } from "@mui/x-data-grid";
 import "./dataTable.scss"
-import { Grid } from "@mui/material";
+import { Link } from "react-router-dom";
 
+type Props = {
+    columns: GridColDef[],
+    rows: Object[],
+    slug: string
 
+}
 
-const DataTable = () => {
+const DataTable = (props: Props) => {
 
-    const columns: GridColDef[] = [
-        { field: 'id', headerName: 'ID', width: 90 },
-        { 
-            field: 'avatar', headerName: 'Avatar', width: 100,
-            renderCell: (params) => (
-                <img src={params.row.img || "/noavatar.png"} alt=""/>
-            )
-         },
-         { 
-            field: 'action', headerName: 'Action', width: 100,
-            renderCell: (params) => {
-                return <div className="action">
-                    <div className="view">View</div>
-                    <div className="delete">Delete</div>
+    const handleDelete = (id: number) => {
+        //delete the item
+        console.log(id + "has been deleted")
+      };
+
+    const actionColumn: GridColDef = {
+        field: "action",
+        headerName: "Action",
+        width: 200,
+        renderCell: (params) => {
+            return (
+              <div className="action">
+                <Link to={`/${props.slug}/${params.row.id}`}>
+                  <img src="/view.svg" alt="" />
+                </Link>
+                <div className="delete" onClick={() => handleDelete(params.row.id)}>
+                  <img src="/delete.svg" alt="" />
                 </div>
-            }
-         },
-         { field: 'status', headerName: 'Status', width: 100, type: 'boolean' },
-        {
-          field: 'firstName',
-          headerName: 'First name',
-          width: 150,
-          editable: true,
-        },
-        {
-          field: 'lastName',
-          headerName: 'Last name',
-          width: 150,
-          editable: true,
-        },
-        {
-          field: 'age',
-          headerName: 'Age',
-          type: 'number',
-          width: 110,
-          editable: true,
-        },
-        {
-          field: 'fullName',
-          headerName: 'Full name',
-          description: 'This column has a value getter and is not sortable.',
-          sortable: false,
-          width: 160,
-          valueGetter: (params: GridValueGetterParams) =>
-            `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-        },
-      ];
-      
-      const rows = [
-        { id: 1, lastName: 'Snow', firstName: 'Jon', age: 14, status: true },
-        { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 31, status: false },
-        { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 31 },
-        { id: 4, lastName: 'Stark', firstName: 'Arya', age: 11 },
-        { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-        { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-        { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-        { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-        { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-      ];
+              </div>
+            );
+          },
+    }
 
   return (
     <div className="dataTable">
         <DataGrid
         className="dataGrid"
-        rows={rows}
-        columns={columns}
+        rows={props.rows}
+        columns={[...props.columns, actionColumn]}
         initialState={{
           pagination: {
             paginationModel: {
-              pageSize: 5,
+              pageSize: 10,
             },
           },
         }}
--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,14 +1,65 @@
+import { GridColDef } from "@mui/x-data-grid";
 import DataTable from "../../components/dataTable/DataTable"
 import "./users.scss"
+import { userRows } from "../../data";
+import { useState } from "react";
+import Add from "../../components/add/Add";
+
+const columns: GridColDef[] = [
+  { field: 'id', headerName: 'ID', width: 90 },
+  { 
+      field: 'img', headerName: 'Avatar', width: 100,
+      renderCell: (params) => (
+          <img src={params.row.img || "/noavatar.png"} alt=""/>
+      )
+   },
+  {
+    field: 'firstName',
+    headerName: 'First name',
+    width: 150,
+  },
+  {
+    field: 'lastName',
+    headerName: 'Last name',
+    width: 150,
+  },
+  {
+    field: 'email',
+    headerName: 'Email',
+    type: 'string',
+    width: 200,
+  },
+  {
+    field: 'phone',
+    headerName: 'Phone',
+    type: 'string',
+    width: 200,
+  },
+  {
+    field: 'createdAt',
+    headerName: 'Created At',
+    type: 'string',
+    width: 200,
+  },
+  {
+    field: 'verified',
+    headerName: 'Verified',
+    type: 'string',
+    width: 100,
+  },
+  { field: 'status', headerName: 'Status', width: 100, type: 'boolean' },
+];
 
 const Users = () => {
+  const [open, setOpen] = useState(false)
   return (
     <div className="users">
       <div className="info">
         <h1>Users</h1>
-        <button>Add new users</button>
+        <button onClick={() => setOpen(true)}>Add new users</button>
       </div>
-      <DataTable />
+      <DataTable slug="users" columns={columns} rows={userRows} />
+      {open && <Add slug="users" columns={columns} setOpen={setOpen} />}
     </div>
   )
 }
