ab27fcf2ac92ce35c0725f3779567fd80cc571c2
rahul verma <rahul.verma40@freecharge.com>
2024-01-25 11:40:21 +0530

    Add BarChartBox component and its styles

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,6 +1,15 @@
 import ChartBox from "../../components/chartBox/ChartBox"
 import TopBox from "../../components/topBox/TopBox"
 import "./home.scss"
+import {
+  barChartBoxRevenue,
+  barChartBoxVisit,
+  chartBoxConversion,
+  chartBoxProduct,
+  chartBoxRevenue,
+  chartBoxUser,
+} from "../../data";
+import BarChartBox from "../../components/barChartBox/BarChartBox";
 
 const Home = () => {
   return (
@@ -8,14 +17,14 @@ const Home = () => {
       <div className="box box1">
         <TopBox />
       </div>
-      <div className="box box2"><ChartBox /></div>
-      <div className="box box3"><ChartBox /></div>
+      <div className="box box2"><ChartBox {...chartBoxUser} /></div>
+      <div className="box box3"><ChartBox {...chartBoxProduct} /></div>
       <div className="box box4">Box4</div>
-      <div className="box box5"><ChartBox /></div>
-      <div className="box box6"><ChartBox /></div>
+      <div className="box box5"><ChartBox {...chartBoxRevenue} /></div>
+      <div className="box box6"><ChartBox {...chartBoxConversion} /></div>
       <div className="box box7">Box7</div>
-      <div className="box box8">Box8</div>
-      <div className="box box9">Box9</div>
+      <div className="box box8"><BarChartBox {...barChartBoxVisit} /></div>
+      <div className="box box9"><BarChartBox {...barChartBoxRevenue} /></div>
 
     </div>
   )
